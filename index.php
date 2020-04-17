af0b1a32f4fe11ae2182169d31394dc392b75fa1
Aaron Tadina <atadina@mail.greenriver.edu>
2020-04-17 16:13:00 -0400

    Added index page

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/* Aaron Tadina
+ * 04/17/20
+ * aatadina.greenriverdev.com/328/pp2
+ * Pair Program 2
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+</head>
+<body>
+<h1>Pair Program 2</h1>
+<?
+echo "PHP Array Practice"
+?>
+</body>
+</html>
